1b4761e6ac92a00189d53b2f5a04214e396f328a
Rafael <54147384+RafaRpso@users.noreply.github.com>
2022-03-28 22:35:28 +0000

    pequenas correções nos nomes /add foreignkey
    corrigi os nomes (lixeira -> tbLixeira) e mudei os tamanhos dfe algumas varchars. Adicionei foreign key também, que estavam apenas em "teoria"

--- arquivos sprint 1/script_banco_ecotech.sql
+++ arquivos sprint 1/script_banco_ecotech.sql
@@ -5,7 +5,7 @@ CREATE DATABASE EcoTech;
 USE EcoTech;
 
 -- Criando a tabela EnderecoEmpresa
-CREATE TABLE EnderecoEmpresa (
+CREATE TABLE tbEnderecoEmpresa (
 	idEnderecoEmpresa INT PRIMARY KEY AUTO_INCREMENT,
     cepEnderecoEmpresa CHAR (9),
     ruaEnderecoEmpresa VARCHAR (100),
@@ -15,50 +15,52 @@ CREATE TABLE EnderecoEmpresa (
 );
 
 -- Criando a tabela Empresa
-CREATE TABLE Empresa (
+CREATE TABLE tbEmpresa (
 	idEmpresa INT PRIMARY KEY AUTO_INCREMENT,
     nomeEmpresa VARCHAR (80),
     cnpjEmpresa CHAR (18),
-    idEnderecoEmpresa INT -- Será a chave estrangeira que ligará a tabela de enredeço e a tabela empresa
+    idEnderecoEmpresa INT, -- Será a chave estrangeira que ligará a tabela de enredeço e a tabela empresa
+    FOREIGN KEY (idEnderecoEmpresa) REFERENCES tbEnderecoEmpresa(idEnderecoEmpresa)
 );
 
 -- Criando a tabela Lixeira
-CREATE TABLE Lixeira (
+CREATE TABLE tbLixeira (
 	idLixeira INT PRIMARY KEY AUTO_INCREMENT,
-    tipoLixeira VARCHAR (8),
-    capacidadeLixeira VARCHAR (6),
+    tipoLixeira VARCHAR (20), -- Pode ser radioativa, etc. Adicionei 20
+    capacidadeLixeira VARCHAR (20), -- Não entendi. Retorna valores numéricos ou de strings (cheios, médio,etc)? Adicionei pra 20 considerando 2° hipotese
     nivelLixeira INT,
-    localizacaoLixeira VARCHAR (50),
-    idEmpresa INT -- Será a chave estrangeria que ligará a tabela empresa e a tabela lixeira
+    localizacaoLixeira VARCHAR (30),
+    idEmpresa INT,
+    FOREIGN KEY (idEmpresa) REFERENCES tbEmpresa(idEmpresa) -- Será a chave estrangeria que ligará a tabela empresa e a tabela lixeira
 );
 
 -- Inserindo dados na tabela EnderecoEmpresa
-INSERT INTO EnderecoEmpresa (cepEnderecoEmpresa, ruaEnderecoEmpresa, numEnderecoEmpresa, bairroEnderecoEmpresa, cidadeEnderecoEmpresa)
+INSERT INTO tbEnderecoEmpresa (cepEnderecoEmpresa, ruaEnderecoEmpresa, numEnderecoEmpresa, bairroEnderecoEmpresa, cidadeEnderecoEmpresa)
 	VALUES ('01311-300', 'Avenida Paulista', '2073', 'Consolação', 'São Paulo'),
 			('03031-040', 'Rua Hannemann', '415', 'Canindé', 'São Paulo'),
             ('01311-200', 'Avenida Paulista', '1337', 'Bela Vista', 'São Paulo');
             
 -- Inserindo dados na tabela Empresa
-INSERT INTO Empresa (nomeEmpresa, cnpjEmpresa, idEnderecoEmpresa)
+INSERT INTO tbEmpresa (nomeEmpresa, cnpjEmpresa, idEnderecoEmpresa)
 	VALUES ('Carrefour Express Conjunto Nacional', '45.543.915/0001-81', 1),
 			('Galeria Pagé Brás', '26.425.597/0001-00', 2),
             ('Edifício Paulista I', '02.494.013/0001-90', 3);
             
-INSERT INTO Lixeira (tipoLixeira, capacidadeLixeira, nivelLixeira, localizacaoLixeira, idEmpresa)
+INSERT INTO tbLixeira (tipoLixeira, capacidadeLixeira, nivelLixeira, localizacaoLixeira, idEmpresa)
 	VALUES ('Metal', '50 L', 2, 'Entrada Loja', 1),
 			('Plástico', '50 L', 1, 'Entrada Loja', 1),
             ('Papel', '50 L', 0, 'Entrada Loja', 1),
             ('Vidro', '50 L', 0, 'Entrada Loja', 1),
-            ('Metal', '50 L', 0, '2º Andar Bloco A', 2),
+            ('Plástico', '50 L', 0, '2º Andar Bloco A', 2),
 			('Plástico', '50 L', 3, '2º Andar Bloco A', 2),
             ('Papel', '50 L', 2, '2º Andar Bloco A', 2),
             ('Vidro', '50 L', 0, '2º Andar Bloco A', 2),
             ('Metal', '50 L', 0, '15º Andar', 3),
 			('Plástico', '50 L', 1, '15º Andar', 3),
             ('Papel', '50 L', 3, '15º Andar', 3),
-            ('Vidro', '50 L', 0, '15º Andar', 3);
+            ('Orgânico', '50 L', 0, '15º Andar', 3);
 		
-SELECT * FROM EnderecoEmpresa;
-SELECT * FROM Empresa;
-SELECT * FROM Lixeira;
+SELECT * FROM tbEnderecoEmpresa;
+SELECT * FROM tbEmpresa;
+SELECT * FROM tbLixeira;
             
