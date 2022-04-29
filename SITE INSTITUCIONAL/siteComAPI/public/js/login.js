c498796eaf9b81ca3bbb249d06f88dee388777dd
GuilhermeAlvesBarros <guilherme.abarros@sptech.school>
2022-04-28 22:09:33 -0300

    site com API

---
+++ SITE INSTITUCIONAL/siteComAPI/public/js/login.js
@@ -0,0 +1,72 @@
+function entrar() {
+    
+
+    var formulario = new URLSearchParams(new FormData(document.getElementById("form_login")));
+
+    console.log("FORM LOGIN: ", formulario.get("login"));
+    console.log("FORM SENHA: ", formulario.get("senha"));
+
+    fetch("/usuarios/autenticar", {
+        method: "POST",
+        body: formulario
+    }).then(function (resposta) {
+        console.log("ESTOU NO THEN DO entrar()!")
+
+        if (resposta.ok) {
+            console.log(resposta);
+
+            resposta.json().then(json => {
+                console.log(json);
+                console.log(JSON.stringify(json));
+
+                sessionStorage.LOGIN_USUARIO = json.login;
+                sessionStorage.NOME_USUARIO = json.nomeEmpresa;
+                sessionStorage.ID_USUARIO = json.id;
+
+                setTimeout(function () {
+                    window.location = "/index.html";
+                }, 1000);
+            });
+
+        } else {
+
+            console.log("Erro de login!");
+
+            resposta.text().then(texto => {
+                console.error(texto);
+                // limparFormulario();
+                
+            });
+        }
+
+    }).catch(function (erro) {
+        console.log(erro);
+    })
+
+    return false;
+}
+
+function validarSessao() {
+    
+
+    var login = sessionStorage.LOGIN_USUARIO;
+    var nomeEmpresa = sessionStorage.NOME_USUARIO;
+
+    var h1Titulo = document.getElementById("h1_titulo");
+
+    if (login != null && nomeEmpresa != null) {
+        // window.alert(`Seja bem-vindo, ${nome}!`);
+        h1Titulo.innerHTML = `${login}`;
+
+        
+    } else {
+        window.location = "login.html";
+    }
+}
+
+function sair() {
+    
+    sessionStorage.clear();
+    
+    window.location = "login.html";  
+}
