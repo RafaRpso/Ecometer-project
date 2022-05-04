f7acfa6c1aafe2a204fdc6fb4dc769920d53a0db
JGbarreto <joao.conceicao@sptech.school>
2022-05-04 15:57:45 -0300

    Alteração texto

--- SITE INSTITUCIONAL/siteComAPI/public/js/graficoChart.js
+++ SITE INSTITUCIONAL/siteComAPI/public/js/graficoChart.js
@@ -787,8 +787,8 @@ function selecaoKitAlerta(kit) { // recebe o valor do kit de outra funcao, da fu
     else if (kit== "2" ) { // kit 2 - qUASE VAZIO  
       
         textoKit.innerHTML = "Kit 2"
-        condicaoKit.innerHTML = "<b> O Kit 2 está quase cheio! </b>"
-        situacao.innerHTML = "<b> O Kit 2 está quase cheio! </b>"
+        condicaoKit.innerHTML = "<b> O Kit 2 está pouco cheio! </b>"
+        situacao.innerHTML = "<b> O Kit 2 está pouco cheio! </b>"
         aviso1.style.display = 'none'
         aviso2.style.display = 'block'
         aviso3.style.display = 'none'
